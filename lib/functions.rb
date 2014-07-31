629f9a46e34eb37b54e763da026f8a83666f54a4
amitbadheka <ab@pykih.com>
2014-07-31 14:41:34 +0530

    initial commt

---
+++ lib/functions.rb
@@ -0,0 +1,18 @@
+require "functions/version"
+
+class String
+  def chota
+    self.downcase
+  end
+  def mota
+    self.upcase
+  end
+end
+
+class Fixnum
+  def divide2
+    self/2
+  end
+end
+
+  
